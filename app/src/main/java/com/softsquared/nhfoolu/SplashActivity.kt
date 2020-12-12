6124f426ae345cf4380230da3b051f6d3746b786
rockdam <choi37338@naver.com>
2020-12-12 17:58:04 +0900

    Initial commit

---
+++ app/src/main/java/com/softsquared/nhfoolu/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.softsquared.nhfoolu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity :AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
